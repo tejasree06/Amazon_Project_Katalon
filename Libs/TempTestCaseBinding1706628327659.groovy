54140eec5d606db4f11adeb0714e0fe3670d2182
tejasree06 <139046615+tejasree06@users.noreply.github.com>
2024-03-20 10:39:59 +0530

    Add files via upload

---
+++ Libs/TempTestCaseBinding1706628327659.groovy
@@ -0,0 +1,26 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\jithe\\AppData\\Local\\Temp\\Katalon\\Test Cases\\Manual\\TC_Login_Manual\\20240130_205527\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+       TestCaseMain.startTestCaseBinding('Test Cases/Manual/TC_Login_Manual', new File("C:\\Users\\jithe\\AppData\\Local\\Temp\\Katalon\\Test Cases\\Manual\\TC_Login_Manual\\20240130_205528\\testCaseBinding"))
+    
